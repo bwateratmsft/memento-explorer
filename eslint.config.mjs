fa629e3e68d7915c46bba768c41d2b71b8a4da9e
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2026-03-16 16:32:04 -0400

    Config fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,38 @@
 // Copyright Brandon Waterloo. All rights reserved.
 // Licensed under the MIT license.
 
-export { azExtEslintRecommendedTypeChecked as default } from '@microsoft/vscode-azext-eng/eslint';
+import { azExtEslintRecommendedTypeChecked } from '@microsoft/vscode-azext-eng/eslint';
+import eslintPluginHeader from '@tony.ganchev/eslint-plugin-header';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig(
+    azExtEslintRecommendedTypeChecked,
+    {
+        plugins: {
+            'header': eslintPluginHeader,
+        },
+        rules: {
+            'header/header': [
+                'error',
+                {
+                    header: {
+                        commentType: 'line',
+                        lines: [
+                            {
+                                pattern: /Brandon Waterloo/,
+                                template: ' Copyright Brandon Waterloo. All rights reserved.',
+                            },
+                            {
+                                pattern: /LICENSE/i,
+                                template: ' Licensed under the MIT license.',
+                            },
+                        ],
+                    },
+                    trailingEmptyLines: {
+                        minimum: 2,
+                    },
+                },
+            ],
+        },
+    }
+)
